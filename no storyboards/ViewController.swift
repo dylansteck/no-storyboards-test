123322eae3533fb4675c9061114e78bddc9e2fa7
Dylan Steck <dylancsteck@gmail.com>
2017-04-08 10:28:16 -0400

    initial

--- no storyboards/ViewController.swift
+++ no storyboards/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var window: UIWindow?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.view.backgroundColor = UIColor.white
+        let myView: UIView = UIView(frame: CGRect(x:0, y:20, width: (self.window!.frame.width), height: 200))
+        myView.backgroundColor = UIColor.blue
+        self.view.addSubview(myView)
+        
+        let myLabel = UILabel(frame: CGRect(x: 200, y: 100, width: 180, height: 50))
+        myLabel.text = "This is test text."
+        myLabel.textColor = UIColor.white
+        self.view.addSubview(myLabel)
+     
     }
 
     override func didReceiveMemoryWarning() {
